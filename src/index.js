7d0a8a0a50fe3e3d3d4ff4ec46537b98cda95eda
Angelica Garcia <anyegar36@gmail.com>
2022-09-29 22:38:37 -0500

    Se prueba la funcion isValid

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 import validator from './validator.js';
 
-console.log(validator);
+const $ = (selector) => document.querySelector(selector)
+
+$('#form').addEventListener('submit', (e) => {
+  e.preventDefault()
+  const data = Object.fromEntries(new FormData(e.target))
+  if(validator.isValid(data.number)){
+    alert('Tarjeta valida')
+  }
+})
