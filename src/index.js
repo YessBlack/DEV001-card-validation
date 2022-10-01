51df1194b8c7c97711951d7fc255b34fb56f8b40
Angelica Garcia <anyegar36@gmail.com>
2022-09-30 20:05:26 -0500

    Se realiza la funcionalidad del boton validar

--- src/index.js
+++ src/index.js
@@ -4,8 +4,15 @@ const $ = (selector) => document.querySelector(selector)
 
 $('#form').addEventListener('submit', (e) => {
   e.preventDefault()
-  const data = Object.fromEntries(new FormData(e.target))
-  if(validator.isValid(data.number)){
-    alert('Tarjeta valida')
-  }
+  const data = Object.fromEntries(new FormData(e.target));
+
+  (validator.isValid(data.number))
+  ? alert('Tarjeta valida')
+  : alert('Tarjeta invalida')
+
+  $('#number').value = validator.maskify(data.number)
+
+  alert(validator.maskify(data.number))
+
 })
+
