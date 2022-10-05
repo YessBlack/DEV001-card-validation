91d86cfa6fbadba0870ecd5d65ac39d40644e67a
Angelica Garcia <anyegar36@gmail.com>
2022-10-05 18:52:59 -0500

    Se crear las funciones showAlertSuccess y showAlertDanger

--- src/index.js
+++ src/index.js
@@ -2,13 +2,30 @@ import validator from './validator.js';
 
 const $ = (selector) => document.querySelector(selector)
 
+const showAlertDanger = () => {
+  setTimeout(() => {
+    $('.alert-danger').classList.remove('hide')
+  },2000);
+  setTimeout(() => {
+    $('.alert-danger').classList.add('hide');
+  }, 5000);
+}
+const showAlertSuccess = () => {
+  setTimeout(() => {
+    $('.group-form').disabled = true;
+    $('.alert-success').classList.remove('hide')
+    $('.card').classList.add('blur')
+    $('.section-form').classList.add('blur')
+    $('.alert-success').classList.remove('hide');
+  }, 2000);
+}
 $('#form').addEventListener('submit', (e) => {
   e.preventDefault()
   const data = Object.fromEntries(new FormData(e.target));
 
   (validator.isValid(data.number))
-  ? alert('Felicitaciones haz adquirido una suscripcion de 1 año')
-  : alert('El pago no se ha podido efectuar, por favor verifica los datos ingresados')
+  ? showAlertSuccess()
+  : showAlertDanger();
 
   $('#number').value = validator.maskify(data.number)
 })
