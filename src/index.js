551dd0e6c99b3fb2de77ac442ad71d0d2e52edeb
Angelica Garcia <anyegar36@gmail.com>
2022-10-05 21:28:35 -0500

    Ajustes en alertas y validacion de inputs

--- src/index.js
+++ src/index.js
@@ -2,7 +2,9 @@ import validator from './validator.js';
 
 const $ = (selector) => document.querySelector(selector)
 
-const showAlertDanger = () => {
+//Mostar Alerta de Error
+const showAlertDanger = (text) => {
+  $('.text-danger').textContent = text
   setTimeout(() => {
     $('.alert-danger').classList.remove('hide')
   },2000);
@@ -10,6 +12,7 @@ const showAlertDanger = () => {
     $('.alert-danger').classList.add('hide');
   }, 5000);
 }
+//Mostar Alerta de Success
 const showAlertSuccess = () => {
   setTimeout(() => {
     $('.group-form').disabled = true;
@@ -19,22 +22,33 @@ const showAlertSuccess = () => {
     $('.alert-success').classList.remove('hide');
   }, 2000);
 }
+//Recuperar datos del formulario
 $('#form').addEventListener('submit', (e) => {
   e.preventDefault()
   const data = Object.fromEntries(new FormData(e.target));
 
-  (validator.isValid(data.number))
-  ? showAlertSuccess()
-  : showAlertDanger();
+  if(data.number <= 0) {
+    showAlertDanger('Por favor ingrese un número válido');
+  }else{
+    (validator.isValid(data.number))
+    ? showAlertSuccess()
+    : showAlertDanger('El pago no se ha podido efectuar, por favor verifica los datos ingresados');
+  }
 
   $('#number').value = validator.maskify(data.number)
 })
 
+//Eventos keyup
 $('#name').addEventListener('keyup', () => {
   $('.name').innerHTML = $('#name').value.toUpperCase()
 })
 
 $('#number').addEventListener('keyup', () => {
+  $('#number').value = $('#number').value
+  .replace(/\s/g,'')
+  .replace(/\D/g,'')
+  .trim()
+
   $('.end-number').innerHTML = $('#number').value.slice(-4)
 })
 
